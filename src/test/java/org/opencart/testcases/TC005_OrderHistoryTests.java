5a7283e40302b16a5b8ba2bd0f4ae11fc3488cd8
Abhishek-TestAutomation <abhishekraisahil220797@gmail.com>
2026-07-22 18:39:30 +0530

    Modified OrderHistoryTest

--- src/test/java/org/opencart/testcases/TC005_OrderHistoryTests.java
+++ src/test/java/org/opencart/testcases/TC005_OrderHistoryTests.java
@@ -1,1 +1,11 @@
+package org.opencart.testcases;
 
+import org.opencart.base.BaseTest;
+
+public class TC005_OrderHistoryTests extends BaseTest{
+	
+	public void verifyOrderHistoryPage() {
+		
+	}
+	
+}
