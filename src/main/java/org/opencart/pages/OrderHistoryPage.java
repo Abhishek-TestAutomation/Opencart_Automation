4166fbca1675a47ff0ea52ade5d8e25f936ba39c
Abhishek-TestAutomation <abhishekraisahil220797@gmail.com>
2026-07-22 18:27:26 +0530

    Modified OrderHistoryPage and added constructor

--- src/main/java/org/opencart/pages/OrderHistoryPage.java
+++ src/main/java/org/opencart/pages/OrderHistoryPage.java
@@ -1,1 +1,12 @@
+package org.opencart.pages;
 
+import org.opencart.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class OrderHistoryPage extends BasePage{
+	
+	public OrderHistoryPage(WebDriver driver) {
+		super(driver);
+	}
+	
+}
